7981dafd2a99b373388eb4858def00fbf0837a54
Andrew PV <112729614+open230884@users.noreply.github.com>
2022-09-28 12:54:38 +0300

    написал рабочий код

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine();
+
+Console.WriteLine("Введите трёхзначное число, далее программа покажет последнюю цифру этого числа:");
+
+int n = int.Parse(Console.ReadLine());
+
+// Проверка на соответсвие требованиям ввода:
+
+if (n > 999 || n < 100)
+{
+    Console.WriteLine("Вы ввели неверное число");
+}
+    // Сама процедура обработки и вывода опрерации
+     else 
+    {
+        Console.WriteLine("последняя цифра этого числа:");
+
+        int j = (n - ((n / 10)*10));
+
+        Console.WriteLine(j);
+    }
